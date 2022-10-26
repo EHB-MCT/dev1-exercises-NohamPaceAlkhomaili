0a28f5fc0b196db900ff3a0dcf23b88a4652bac5
noham <noham.pace.alkhomaili@student.ehb.be>
2022-10-26 12:04:01 +0200

    exercise

---
+++ exercise/4-iteration/4-4 Circles, Circles, Circles.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import context from "../../scripts/context.js"
+import * as Utils from "../../scripts/utils.js";
+
+
+
+
+for (let i = 0; i < 3; i++)
+{
+for (let j = 0; j < 3; j++)
+{
+console.log(i + j);
+}
+}
